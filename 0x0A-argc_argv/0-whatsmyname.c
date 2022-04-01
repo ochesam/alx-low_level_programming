88e8f6ed9f56815baed8d47b299056bc0884e589
John samuel <99280541+ochesam@users.noreply.github.com>
2022-04-01 17:02:00 +0100

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,33 +1,16 @@
-#include "main.h"
-
+#include "holberton.h"
 #include <stdio.h>
 
-
-
 /**
+ * main - prints buffer in hexa
+ * @argc: he number of command line arguments
+ * @argv: An array containing the program command line arguments.
  *
- *  * main - prints buffer in hexa
- *
- *   * @argc: he number of command line arguments
- *
- *    * @argv: An array containing the program command line arguments.
- *
- *     *
- *
- *      * Return: Nothing.
- *
- *       */
-
+ * Return: Nothing.
+ */
 int main(int argc, char *argv[])
-
 {
-
-		(void)argc;
-
-			printf("%s\n", argv[0]);
-
-				return (0);
-
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
 }
-
-
