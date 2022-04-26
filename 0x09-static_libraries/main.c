6252fca408431d6ade0583fc9cf800a6e8231946
ochesam <ochesam822@gmail.com>
2022-04-25 18:47:30 -0700

    0x13-more_singly_linked_lists

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,15 @@
+nclude "main.h"                                                                                                                                                                     
+/**                                                                                                                                                                                   
+ * main - Entry point                                                                                                                                                                 
+ *   *                                                                                                                                                                                    
+ * Return: Always 0 (Success)                                                                                                                                                         
+ */                                                                                                                                                                                   
+int main(void)                                                                                                                                                                        
+{                                                                                                                                                                                     
+	            _puts("\"At the end of the day, my goal was to be the best hacked\"\n");       
+		    _puts("\t - Kevin Mitnick");
+		    return(0);
+
+
+		                                                                                                                                                           
+}
